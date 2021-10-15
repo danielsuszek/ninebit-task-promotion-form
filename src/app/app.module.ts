708462395e9cf08721ae72a3b4bdc7d5ec6ef66b
danielsuszek <danielsuszek1@gmail.com>
2021-10-15 12:29:10 +0200

    Add all empty componets to layout.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormPromotionModule } from './form-promotion/form-promotion.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormPromotionModule
   ],
   providers: [],
   bootstrap: [AppComponent]
