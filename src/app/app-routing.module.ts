8b18d9c5705ce416b38cbc6616bc39a34d7f4028
danielsuszek <danielsuszek1@gmail.com>
2021-10-16 22:14:22 +0200

    Fixed app.component.html.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
+import { MainFormComponent } from './form-promotion/main-form/main-form.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: MainFormComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
