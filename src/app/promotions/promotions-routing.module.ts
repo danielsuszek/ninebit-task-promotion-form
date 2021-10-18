adf5c1992e3b087415731160b3f1539a6475e174
danielsuszek <danielsuszek1@gmail.com>
2021-10-18 11:52:22 +0200

    Create all promotion components for CRUD with routes.

--- src/app/promotions/promotions-routing.module.ts
+++ src/app/promotions/promotions-routing.module.ts
@@ -1,3 +1,6 @@
+import { UpdatePromotionComponent } from './update-promotion/update-promotion.component';
+import { DeletePromotionComponent } from './delete-promotion/delete-promotion.component';
+import { GetAllPromotionsComponent } from './get-all-promotions/get-all-promotions.component';
 import { CreatePromotionComponent } from './create-promotion/create-promotion.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
@@ -5,7 +8,10 @@ import { PromotionsComponent } from './promotions.component';
 
 const routes: Routes = [
   { path: '', component: PromotionsComponent },
-  { path: 'create-promotion', component: CreatePromotionComponent },
+  { path: 'create', component: CreatePromotionComponent },
+  { path: 'list', component: GetAllPromotionsComponent },
+  { path: 'update', component: UpdatePromotionComponent },
+  { path: 'delete', component: DeletePromotionComponent },
 
 ];
 
